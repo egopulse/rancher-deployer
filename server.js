ce6bbd4db028d6365edf301cee6c28d4e1d888ca
Lagz0ne <duc@outlook.sg>
2016-09-06 11:16:44 +0700

    Added slack integration

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const {
   RANCHER_ACCESS_KEY,
   RANCHER_SECRET_KEY,
   RANCHER_STACK_ID,
-  RANCHER_STACK_NAME
+  RANCHER_STACK_NAME,
+  SLACK_WEBHOOK
 } = process.env;
 
 const express = require('express');
@@ -20,14 +21,23 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  res.send('Upgrading');
+  res.send(`Upgrading ${req.query.commit}`);
 
   const PROJECT_CONFIG_URL = `${RANCHER_URL}/environments/${RANCHER_STACK_ID}/composeconfig`;
   
   exec(`curl -s -L -u "${RANCHER_ACCESS_KEY}:${RANCHER_SECRET_KEY}" ${PROJECT_CONFIG_URL} -o ${__dirname}/config.zip`);
   exec(`unzip -o ${__dirname}/config.zip`);
   rm(`${__dirname}/config.zip`);
-  exec(`cd ${__dirname} && ./rancher-compose -p ${RANCHER_STACK_NAME} up --force-upgrade --confirm-upgrade --pull -d`);
+
+  const upgradeResult = exec(`cd ${__dirname} && ./rancher-compose -p ${RANCHER_STACK_NAME} up --force-upgrade --confirm-upgrade --pull -d`);
+
+  if (SLACK_WEBHOOK) {
+    if (upgradeResult.code !== 0) {
+      exec(`curl -X POST -H 'Content-type: application/json' --data '{"text":"Failed to upgrade staging to ${req.query.commit} \n ${upgradeResult.stdout} \n ${upgradeResult.stderr}"}' ${SLACK_WEBHOOK}`);
+    } else {
+      exec(`curl -X POST -H 'Content-type: application/json' --data '{"text":"Staging is on ${req.query.commit}"}' ${SLACK_WEBHOOK}`);
+    }
+  }
 });
 
 app.listen(PORT, function () {
