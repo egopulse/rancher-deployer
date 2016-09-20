fe1d489850e8cdf0c8b075bcce157d0d21dc4fef
Lagz0ne <duc@outlook.sg>
2016-09-20 16:36:56 +0700

    Added docker ignore, remove wrong files

--- server.js
+++ server.js
@@ -14,29 +14,33 @@ const express = require('express');
 const app = express();
 const dir = __dirname;
 
-app.get('/', (req, res) => {
-  res.send('Nothing to look for here');
-  exec('sleep 10');
-  echo('test');
-});
+let isUpgrading = false;
 
 app.post('/', (req, res) => {
-  res.send(`Upgrading ${req.query.commit}`);
+  if (isUpgrading) {
+    res.send(`In middle of another upgrade ...`);
+  } else {
+    isUpgrading = true;
+    res.send(`Upgrading ${req.query.commit}`);
 
-  const PROJECT_CONFIG_URL = `${RANCHER_URL}/environments/${RANCHER_STACK_ID}/composeconfig`;
-  
-  exec(`curl -s -L -u "${RANCHER_ACCESS_KEY}:${RANCHER_SECRET_KEY}" ${PROJECT_CONFIG_URL} -o ${__dirname}/config.zip`);
-  exec(`unzip -o ${__dirname}/config.zip`);
-  rm(`${__dirname}/config.zip`);
+    const PROJECT_CONFIG_URL = `${RANCHER_URL}/environments/${RANCHER_STACK_ID}/composeconfig`;
+    
+    exec(`curl -s -L -u "${RANCHER_ACCESS_KEY}:${RANCHER_SECRET_KEY}" ${PROJECT_CONFIG_URL} -o ${__dirname}/config.zip`);
+    exec(`unzip -o ${__dirname}/config.zip`);
+    rm(`${__dirname}/config.zip`);
 
-  const upgradeResult = exec(`cd ${__dirname} && ./rancher-compose -p ${RANCHER_STACK_NAME} up --force-upgrade --confirm-upgrade --pull -d`);
+    const upgradeResult = exec(`cd ${__dirname} && ./rancher-compose -p ${RANCHER_STACK_NAME} up --force-upgrade --confirm-upgrade --pull -d`);
 
-  if (SLACK_WEBHOOK) {
-    if (upgradeResult.code !== 0) {
-      exec(`curl -X POST -H 'Content-type: application/json' --data '{"text":"Failed to upgrade staging to ${req.query.commit} \n ${upgradeResult.stdout} \n ${upgradeResult.stderr}"}' ${SLACK_WEBHOOK}`);
-    } else {
-      exec(`curl -X POST -H 'Content-type: application/json' --data '{"text":"Staging is on \`${req.query.commit}\`"}' ${SLACK_WEBHOOK}`);
+    if (SLACK_WEBHOOK) {
+      if (upgradeResult.code !== 0) {
+        exec(`curl -X POST -H 'Content-type: application/json' --data '{"text":"Failed to upgrade staging to ${req.query.commit} \n ${upgradeResult.stdout} \n ${upgradeResult.stderr}"}' ${SLACK_WEBHOOK}`);
+      } else {
+        exec(`curl -X POST -H 'Content-type: application/json' --data '{"text":"Staging is on \`${req.query.commit}\`"}' ${SLACK_WEBHOOK}`);
+      }
     }
+
+    rm(`${__dirname}/*.yml`);
+    isUpgrading = false;
   }
 });
 
